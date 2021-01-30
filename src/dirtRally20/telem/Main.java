9c66ffbc3db40b978e86a30ca2ae2edd2acaee5a
MavoSV8 <Lukaszpasi@gmail.com>
2021-01-30 14:24:49 +0100

    Value of velocity now changes in one line, reading and displaying value of RPMs and gears needs to corrected

--- src/dirtRally20/telem/Main.java
+++ src/dirtRally20/telem/Main.java
@@ -1,5 +1,6 @@
 package dirtRally20.telem;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -17,8 +18,14 @@ public class Main {
             dataBuffer = getData.receiveData();
 
             car.setVelocity(Float.intBitsToFloat(dataBuffer[31] << 24 | (dataBuffer[30] & 0xFF) << 16 | (dataBuffer[29] & 0xFF) << 8 | (dataBuffer[28] & 0xFF)));
+            car.setRPMs(Float.intBitsToFloat(dataBuffer[231] << 24 | (dataBuffer[230] & 0xFF) << 16 | (dataBuffer[229] & 0xFF) << 8 | (dataBuffer[228] & 0xFF)));
+            car.setGear(Float.intBitsToFloat(dataBuffer[141] << 24 | (dataBuffer[140] & 0xFF) << 16 | (dataBuffer[139] & 0xFF) << 8 | (dataBuffer[138] & 0xFF)));
             //car.setVelocity(ByteBuffer.wrap(Arrays.copyOfRange(dataBuffer, 28, 32)).getFloat());
-            System.out.println(car.getVelocity()*3.6);
+            System.out.print((int)(car.getVelocity()*3.6) + "  ");
+            System.out.print(car.getRPMs() + "    ");
+            System.out.print(car.getGear() + " ");
+            System.out.print("\r");
+            //System.out.flush();
            /* if (input.nextInt() == 1) {
                 end = false;
             }*/
