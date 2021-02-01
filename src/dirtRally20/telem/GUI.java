d6389e6b53900e2d222f86dc7c696f223dff0ace
MavoSV8 <Lukaszpasi@gmail.com>
2021-02-01 02:41:10 +0100

    Added basic GUI which displays velocity, RPMs and gear

--- src/dirtRally20/telem/GUI.java
+++ src/dirtRally20/telem/GUI.java
@@ -1,4 +1,53 @@
 package dirtRally20.telem;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
+    private JFrame frame = new JFrame();
+    private JLabel velocity = new JLabel();
+    private JLabel RPMs = new JLabel();
+    private JLabel gear = new JLabel();
+
+    public void createWindow(String title,int width,int height) {
+        frame.setTitle(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width, height);
+        frame.setVisible(true);
+        velocity.setFont(new Font("Serif", Font.PLAIN, 30));
+        RPMs.setFont(new Font("Serif", Font.PLAIN, 30));
+        gear.setFont(new Font("Serif", Font.PLAIN, 30));
+        frame.getContentPane().add(BorderLayout.WEST, velocity);
+        frame.getContentPane().add(BorderLayout.CENTER, RPMs);
+        frame.getContentPane().add(BorderLayout.EAST, gear);
+    }
+    public void setVelocity(int v){
+        velocity.setText(Integer.toString(v));
+        JComponent parent = (JComponent) velocity.getParent();
+        if(parent !=null) parent.revalidate();
+    }
+
+    public void setRPMs(int RPM) {
+        RPMs.setText(" " + RPM);
+        JComponent parent = (JComponent) RPMs.getParent();
+        if(parent !=null) parent.revalidate();
+    }
+
+    public void setGear(int g) {
+        if(g == -1){
+            //System.out.print("R ");
+            gear.setText("R");
+        }
+        else if(g == 0){
+            //System.out.print("N ");
+            gear.setText("N");
+        }
+        else {
+            //System.out.print((int) (car.getGear()) + " ");
+            gear.setText(Integer.toString(g));
+        }
+        JComponent parent = (JComponent) gear.getParent();
+        if(parent !=null) parent.revalidate();
+
+    }
 }
--- src/dirtRally20/telem/Main.java
+++ src/dirtRally20/telem/Main.java
@@ -11,32 +11,23 @@ public class Main {
         GetPacketFromGame getData = new GetPacketFromGame();
         //Scanner input = new Scanner(System.in);
         Car car = new Car();
+        GUI gui = new GUI();
+        gui.createWindow("Telemetry tool",768,432);
         boolean end = true;
         int i = 0;
         byte dataBuffer[];
+
+
         while (end == true) {
             dataBuffer = getData.receiveData();
 
             car.setVelocity(Float.intBitsToFloat(dataBuffer[31] << 24 | (dataBuffer[30] & 0xFF) << 16 | (dataBuffer[29] & 0xFF) << 8 | (dataBuffer[28] & 0xFF)));
             car.setRPMs(Float.intBitsToFloat(dataBuffer[151] << 24 | (dataBuffer[150] & 0xFF) << 16 | (dataBuffer[149] & 0xFF) << 8 | (dataBuffer[148] & 0xFF)));
             car.setGear(Float.intBitsToFloat(dataBuffer[135] << 24 | (dataBuffer[134] & 0xFF) << 16 | (dataBuffer[133] & 0xFF) << 8 | (dataBuffer[132] & 0xFF)));
-            //car.setVelocity(ByteBuffer.wrap(Arrays.copyOfRange(dataBuffer, 28, 32)).getFloat());
-            System.out.print("Velocity: ");
-            System.out.print((int)(car.getVelocity()*3.6) + "km/h  ");
-            System.out.print("RPM: ");
-            System.out.print((int)(car.getRPMs()*10) + "/min    ");
-            System.out.print("Gear: ");
-            if((int)car.getGear() == -1){
-                System.out.print("R ");
-            }
-            else if((int)car.getGear() == 0){
-                System.out.print("N ");
-            }
-            else {
-                System.out.print((int) (car.getGear()) + " ");
-            }
-            System.out.print("\r");
-            //System.out.flush();
+            gui.setVelocity((int)(car.getVelocity()*3.6));
+            gui.setRPMs((int)(car.getRPMs()*10));
+            gui.setGear((int)car.getGear());
+
            /* if (input.nextInt() == 1) {
                 end = false;
             }*/
